99808be5a39e001c6c6e68976814d51ef9869730
Volodymyr Bilohorodskyi <niko.fm@gmail.com>
2024-03-27 02:04:52 +0200

    Added js logic to mobile menu, added id to sections

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import modal from './js/modal';
+import { Modal } from './js/modal.js';
 import { loadReviews, loadMore } from './js/reviews.js';
 import slider from './js/slider';
 
 slider('.mySwiper');
 loadReviews('.reviews-list-item');
+console.log(Modal);
+
+globalThis.Modal = Modal;
 globalThis.loadMore = loadMore;
