114ea5001ce700472e765766e7f08fa092541e6d
Volodymyr Bilohorodskyi <niko.fm@gmail.com>
2024-03-27 02:21:23 +0200

    Some fixes

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import { Modal } from './js/modal.js';
 import { loadReviews, loadMore } from './js/reviews.js';
 import slider from './js/slider';
 
-slider('.mySwiper');
-loadReviews('.reviews-list-item');
-console.log(Modal);
-
 globalThis.Modal = Modal;
+
+loadReviews('.reviews-list-item');
 globalThis.loadMore = loadMore;
+
+slider('.mySwiper');
